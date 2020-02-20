bf8942b25064f3e4b77a9cac649b3c1012a296cf
Takeallah <takeallahsrivera@icloud.com>
2020-02-20 14:13:46 -0600

    ClassworkPush2

---
+++ main5.js
@@ -0,0 +1,20 @@
+//Practice 1 
+
+/*### Exercise 1:
+- Use a for loop to print numbers -3 through 8
+*/
+
+
+//Exercise 1 Solution
+for (let k = -3;k <= 8; k++)
+{
+    console.log(k);
+
+}
+
+//Exercise A
+
+/*### Exercise A:
+- Print -10 through 5 using a counter and while loop.
+*/ 
+

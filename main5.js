ee526b7498f7675dff70c3038be18094199cf3cb
Takeallah <takeallahsrivera@icloud.com>
2020-02-20 14:28:15 -0600

    ClassworkPush3

--- main5.js
+++ main5.js
@@ -1,4 +1,4 @@
-//Practice 1 
+//Exercise 1 
 
 /*### Exercise 1:
 - Use a for loop to print numbers -3 through 8
@@ -6,15 +6,55 @@
 
 
 //Exercise 1 Solution
-for (let k = -3;k <= 8; k++)
+/*for (let k = -3;k <= 8; k++)
 {
     console.log(k);
 
 }
+*/
 
 //Exercise A
 
 /*### Exercise A:
 - Print -10 through 5 using a counter and while loop.
 */ 
+
+//Exercise 2
+
+/*### Exercise 2:
+- Use a for loop to count from -5 to 5 three times in a row
+
+*/
+
+/*for (let t = -5;t <= 5; t++)
+{
+    console.log(t);
+   
+
+}
+
+for (let t = -5;t <= 5; t++)
+{
+    console.log(t);
+   
+
+}
+
+for (let t = -5;t <= 5; t++)
+{
+    console.log(t);
+   
+
+}
+*/
+
+//Exercise 3
+
+/*### Exercise 3:
+- Use a for loop to print only even numbers from -50 to 50
+
+*/
+
+
+
 
